bb7ace0d24dd4aa0ca36ea43bc50a7512c7c69f0
emma <emmaabryan@gmail.com>
2025-03-19 20:36:10 +0000

    add api routes

--- app.js
+++ app.js
@@ -39,7 +39,58 @@ app.listen(PORT, () => {
   console.log(`My first Express app - listening on port ${PORT}!`);
 });
 
+app.get('/', (req, res) => {
+  res.send("Blog backend")
+})
+
 app.get('/posts', async (req, res, next) => {
   const posts = await prisma.posts.findMany()
   res.json({ posts: posts })
 })
+
+app.get('/posts/:id', async (req, res, next) => {
+  const post = await prisma.posts.findUnique({
+    where: { id: parseInt(req.params.id) }
+  });
+  res.json({ post: post })
+})
+
+app.get('/posts/:id/comments', async (req, res, next) => {
+  const comments = await prisma.comments.findMany({
+    where: { post_id: parseInt(req.params.post_id) }
+  });
+  res.json({ comments: comments })
+})
+
+app.get('/comments/:comment_id', async (req, res, next) => {
+  const comment = await prisma.comments.findMany({
+    where: { id: parseInt(req.params.post_id) }
+  });
+  res.json({ comment: comment })
+})
+
+app.get('/users', async (req, res, next) => {
+  const users = await prisma.users.findMany();
+  res.json({ users: users })
+});
+
+app.get('/users/:id', async (req, res, next) => {
+  const user = await prisma.users.findMany({
+    where: { id: req.params.id }
+  });
+  res.json({ user: user })
+});
+
+app.get('/users/:id/comments', async (req, res, next) => {
+  const comments = await prisma.comments.findMany({
+    where: { user_id: req.params.id }
+  });
+  res.json({ comments: comments })
+});
+
+app.get('/users/:id/posts', async (req, res, next) => {
+  const posts = await prisma.posts.findMany({
+    where: { user_id: req.params.id }
+  });
+  res.json({ posts: posts })
+});
