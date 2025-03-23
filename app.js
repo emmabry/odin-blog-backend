a526ab5d30fdfe9f67451882ed4c539adfd73fa9
emma <emmaabryan@gmail.com>
2025-03-23 13:00:10 +0000

    add login

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const session = require('express-session');
 const { PrismaSessionStore } = require('@quixo3/prisma-session-store');
 const prisma = require('./prismaClient'); 
 const passport = require('passport')
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken')
 const LocalStrategy = require('passport-local').Strategy;
 require("dotenv").config();
@@ -39,26 +40,73 @@ app.use("/posts/", postsRouter);
 app.use("/comments/", commentsRouter);
 app.use("/users/", usersRouter);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`My first Express app - listening on port ${PORT}!`);
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await prisma.users.findUnique({
+        where: { username }
+    });
+
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        return done(null, false, { message: "Incorrect password" })
+}
+      return done(null, user);
+    } catch(err) {
+      return done(err);
+    }
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await prisma.users.findUnique({
+      where: { id }
+  });
+
+    done(null, user);
+  } catch(err) {
+    done(err);
+  }
 });
 
 app.get('/', (req, res) => {
   res.send("Blog backend")
 })
 
-app.post('/login', (req, res) => {
-  const user = {
-    id: 1,
-    username: 'emma',
-    email: 'emma@bryan.com'
-  }
-  jwt.sign({ user: user }, 'secretkey', (err, token) => {
-    res.json(
-      {token : token}
-    );
-  });
-})
+app.post('/login', (req, res, next) => {
+  passport.authenticate("local", (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
+
+    if (!user) {
+      return res.status(400).json({ message: info.message || 'Authentication failed' });
+    }
+
+    req.logIn(user, (err) => {
+      if (err) {
+        return next(err);
+      }
+
+    const token = jwt.sign({ user: user }, 'secretkey', { expiresIn: '1h' });
+
+    return res.json({ token: token });
+    });
+  })(req, res, next);
+});
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`My first Express app - listening on port ${PORT}!`);
+});
 
 // TODO: Require JWT auth for: delete comment, delete post, create post, getAllUsers, deleteUser, createUser, getUserById?
