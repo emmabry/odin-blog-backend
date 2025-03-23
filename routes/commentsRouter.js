cc325a2943dcc110cc9c2ca29cb839fef96c648d
emma <emmaabryan@gmail.com>
2025-03-23 11:48:44 +0000

    add protected routes

--- routes/commentsRouter.js
+++ routes/commentsRouter.js
@@ -1,9 +1,10 @@
 const { Router } = require("express");
 const commentsController = require("../controllers/commentsController");
+const { verifyToken } = require('../middleware/auth'); 
 const commentsRouter = Router();
 
 commentsRouter.get("/:id", commentsController.getCommentById)
 commentsRouter.post("/", commentsController.createComment)
-commentsRouter.delete('/:id', commentsController.deleteComment)
+commentsRouter.delete('/:id', verifyToken, commentsController.deleteComment)
 
 module.exports = commentsRouter;
--- routes/postsRouter.js
+++ routes/postsRouter.js
@@ -1,11 +1,12 @@
 const { Router } = require("express");
 const postsController = require("../controllers/postsController");
+const { verifyToken } = require('../middleware/auth'); 
 const postsRouter = Router();
 
 postsRouter.get("/", postsController.getAllPosts);
 postsRouter.get("/:id", postsController.getPostById)
 postsRouter.get('/:id/comments', postsController.getPostComments)
-postsRouter.post('/', postsController.createPost)
-postsRouter.delete('/:id', postsController.deletePost)
+postsRouter.post('/', verifyToken, postsController.createPost)
+postsRouter.delete('/:id', verifyToken, postsController.deletePost)
 
 module.exports = postsRouter;
